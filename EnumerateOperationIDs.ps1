57ee29852faed6d11ed7f563f28a13f756d0b61b
Nate Barrett <nate_barrett@hillspet.com>
2024-02-26 10:41:13 -0600

    Intial Upload

---
+++ EnumerateOperationIDs.ps1
@@ -0,0 +1,12 @@
+﻿# Define the list of keywords to search for
+$keywords = "USMT"
+
+# Search Active Directory for enabled users whose names contain the keywords
+$users = Get-ADUser -Filter {(Enabled -eq $true) -and (Name -match ($keywords -join "|"))}
+
+# Loop through each user in the list
+foreach ($user in $users) {
+    # Add the user's name to the text file
+    Add-Content -Path "C:\scripts\AD\usmtIDs.txt" -Value $user.Name
+    Write-Host "Added user $($user.Name) to the text file"
+}
